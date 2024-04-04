80bd2350c4b117dc21a46eafa6d06d12eaab7d41
ehsanavy <142739696+ehsanavy@users.noreply.github.com>
2024-04-04 19:55:10 +0330

    Create pro-py-1.py

---
+++ pro-py-1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# ehanavy 2024
+
+
+
+while True:
+	a = int ( input ("number one:"))
+	b = int ( input ("number too:"))
+	c = int (input("number:"))
+	s = b - a
+	d = [0]
+	n = [0]
+	while True:
+		print(b*(n-1)+s)
+		d + [1]
+		n + [1]
+		if d == c:
+			break 
+	#print (a*(2-1)+c)
+	#print (b*(c-1)+s)
+	#print (d)
+	if a == "exit":
+		break
+#t*(n-1)+d		
